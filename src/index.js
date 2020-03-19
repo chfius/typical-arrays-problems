d5452f2eee5f8396cff0943749e3f96a57e0face
Yury <chifius@mail.ru>
2020-03-19 04:23:57 +0300

    feat: finish typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {     
+    if (typeof (array) === 'object') {        
+        if (array.length != 0) {            
+            return Math.min.apply(null, array);} else {return 0;}
+    } else {return 0;}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof (array) === 'object') {
+        if (array.length != 0) {return Math.max.apply(null, array);} else {return 0;}
+    } else {return 0;}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof (array) === 'object') {
+        if (array.length != 0) {return array.reduce((sum, current) => sum + current, 0) / array.length;} else {return 0;}
+    } else {return 0;}
 }
